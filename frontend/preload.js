a80650bd9ba0da38fff1a490aab1ce3e3679368e
Brennan <bthedub@gmail.com>
2023-08-05 01:54:20 +0000

    split js scripts

---
+++ frontend/preload.js
@@ -0,0 +1,28 @@
+// load the tasklist as it currently stands
+function displayTasks() {
+    fetch("/api/tasks")
+        .then(response => response.json())
+        .then(data => {
+            const tasksArray = data.tasks; // Access the tasks array from the data object
+
+            const recordsContainer = document.getElementById("card-list");
+            let recordsHTML = "";
+
+            tasksArray.forEach(task => {
+                recordsHTML += `<div class="card mb-3">
+                    <div class="card-body">
+                        <div>${task.Name}</div>
+                        <button class="btn btn-success">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </div>
+                </div>`;
+                console.log(task);
+            });
+
+            recordsContainer.innerHTML = recordsHTML;
+            console.log(data);
+        })
+        .catch(error => console.error("Error fetching data: ", error));
+}
+
+displayTasks();
